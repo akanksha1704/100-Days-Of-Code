a0b6284a28693f111fb4bfdfdb745d3f899e5504
Akanksha Priya <67060961+akanksha1704@users.noreply.github.com>
2021-01-04 23:30:59 +0530

    Update KadaneLB.cpp

--- KadaneLB.cpp
+++ KadaneLB.cpp
@@ -1,1 +1,24 @@
 
+//find Largest sum contiguous Subarray [V. IMP]
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int a[n];
+
+    for(int i=0;i<n;i++)
+        cin>>a[i];
+
+    int cs=0,ms=INT_MIN;
+    for(int i=0;i<n;i++){
+
+        cs=cs+a[i];
+        if(ms<cs)
+            ms=cs;
+        if(cs<0)
+            cs=0;
+    }
+    cout<<ms<<endl;
+
+return 0; }
