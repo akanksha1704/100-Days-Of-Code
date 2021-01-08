3697fb4e7e30dd3a26633804fc44a68a0b0ad6cd
Akanksha Priya <67060961+akanksha1704@users.noreply.github.com>
2021-01-08 20:27:49 +0530

    Update MergeIntervalLB.cpp

--- MergeIntervalLB.cpp
+++ MergeIntervalLB.cpp
@@ -1,1 +1,47 @@
 
+// Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals, and return an array of the non-overlapping intervals that cover all the intervals in the input.
+
+ 
+
+// Example 1:
+
+// Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
+// Output: [[1,6],[8,10],[15,18]]
+// Explanation: Since intervals [1,3] and [2,6] overlaps, merge them into [1,6].
+// Example 2:
+
+// Input: intervals = [[1,4],[4,5]]
+// Output: [[1,5]]
+// Explanation: Intervals [1,4] and [4,5] are considered overlapping.
+ 
+
+// Constraints:
+
+// 1 <= intervals.length <= 104
+// intervals[i].length == 2
+// 0 <= starti <= endi <= 104
+
+
+
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+       vector<vector<int>>mergeInterval;
+        if(intervals.size()==0)
+            return mergeInterval;
+        vector<int>temp = intervals[0];
+        
+        for(auto it:intervals){
+            if(it[0]<=temp[1]){
+                temp[1]=max(it[1],temp[1]);
+              
+            }
+            else{
+                mergeInterval.push_back(temp);
+                temp=it;
+            }
+        }
+        mergeInterval.push_back(temp);
+        return  mergeInterval;
+    }
+};
