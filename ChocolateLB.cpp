7ee271806ba15c87cb9e82467b22931ccb0939d9
Akanksha Priya <67060961+akanksha1704@users.noreply.github.com>
2021-01-17 20:44:05 +0530

    Update ChocolateLB.cpp

--- ChocolateLB.cpp
+++ ChocolateLB.cpp
@@ -1,1 +1,63 @@
+// Given an array A of positive integers of size N, where each value represents number of chocolates in a packet. Each packet can have variable number of chocolates. There are M students, the task is to distribute chocolate packets such that :
+// 1. Each student gets one packet.
+// 2. The difference between the number of chocolates given to the students having packet with maximum chocolates and student having packet with minimum chocolates is minimum.
 
+// Input:
+// The first line of input contains an integer T, denoting the number of test cases. Then T test cases follow. Each test case consists of three lines. The first line of each test case contains an integer N denoting the number of packets. Then next line contains N space separated values of the array A denoting the values of each packet. The third line of each test case contains an integer m denoting the no of students.
+
+// Output:
+// For each test case in a new line print the minimum difference.
+
+// Constraints:
+// 1 <= T <= 100
+// 1 <=N<= 107
+// 1 <= Ai <= 1018
+// 1 <= M <= N
+
+// Example:
+// Input:
+// 2
+// 8
+// 3 4 1 9 56 7 9 12
+// 5
+// 7
+// 7 3 2 4 9 12 56
+// 3
+
+// Output:
+// 6
+// 2
+
+// Explanation:
+// Testcase 1: The minimum difference between maximum chocolates and minimum chocolates is 9-3=6
+
+
+//T(N)=O(N LOGN)
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	//code
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    int a[n];
+	    for(int i=0;i<n;i++)
+	        cin>>a[i];
+	    int m;
+	    cin>>m;
+	    int d;
+	    int min=INT_MAX;
+	    sort(a,a+n);
+	    for(int i=0;i+m-1<n;i++){
+	        d=a[i+m-1]-a[i];
+	        
+	        if(d<min)
+	            min=d;
+	    }
+	    cout<<min<<endl;
+	}
+	return 0;
+}
