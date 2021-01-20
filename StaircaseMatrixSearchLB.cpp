9aa6cb6d651694e933dc79a9107e22e1a7815bc9
Akanksha Priya <67060961+akanksha1704@users.noreply.github.com>
2021-01-20 22:15:35 +0530

    Update StaircaseMatrixSearchLB.cpp

--- StaircaseMatrixSearchLB.cpp
+++ StaircaseMatrixSearchLB.cpp
@@ -1,1 +1,58 @@
 
+// Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
+
+// Integers in each row are sorted from left to right.
+// The first integer of each row is greater than the last integer of the previous row.
+ 
+
+// Example 1:
+
+
+// Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
+// Output: true
+// Example 2:
+
+
+// Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 13
+// Output: false
+ 
+
+// Constraints:
+
+// m == matrix.length
+// n == matrix[i].length
+// 1 <= m, n <= 100
+// -104 <= matrix[i][j], target <= 104
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n=matrix.size();
+        int m=matrix[0].size();
+        
+        if(n==0 && m==0)
+            return false;
+        
+        int smallest=matrix[0][0];
+        int largest=matrix[n-1][m-1];
+        
+        if(target<smallest || target>largest)
+            return false;
+        
+        int i=0;
+        int j=m-1;
+        while(i<n && j>=0){
+            if(matrix[i][j]==target){
+                return true;
+                break;
+            }
+            else if(matrix[i][j]>target){
+                j--;
+            }
+            else{
+                i++;
+            }
+        }
+        return false;
+    }
+};
