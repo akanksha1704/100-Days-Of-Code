5ca91c3c95254b6c8a40312e17b580f11920ec84
Akanksha Priya <67060961+akanksha1704@users.noreply.github.com>
2021-01-30 20:59:29 +0530

    Create BinaryLB.cpp

---
+++ BinaryLB.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int maxSubStr(string str, int n)
+{
+
+    // To store the count of 0s and 1s
+    int count0 = 0, count1 = 0;
+    int cnt = 0;
+    for (int i = 0; i < n; i++) {
+        if (str[i] == '0') {
+            count0++;
+        }
+        else {
+            count1++;
+        }
+        if (count0 == count1) {
+            cnt++;
+        }
+    }
+
+    // It is not possible to
+    // split the string
+    if (count0 != count1) {
+        return -1;
+    }
+
+    return cnt;
+}
+
+// Driver code
+int main()
+{
+    //string str = "0100110101";
+    string str;
+    cin>>str;
+    int n = str.length();
+
+    cout << maxSubStr(str, n);
+
+    return 0;
+}
