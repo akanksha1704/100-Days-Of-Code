efcc236fd471c5e587d4bb4e705fd9c0ab8ef3a5
Akanksha Priya <67060961+akanksha1704@users.noreply.github.com>
2021-02-10 21:55:55 +0530

    Create knapsackRecursive.cpp

---
+++ knapsackRecursive.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+int knapsack(int wt[],int val[],int w,int n){
+
+    if(n==0 || w==0)
+        return 0;
+    if(wt[n-1]<=w){
+        return max((val[n-1]+knapsack(wt,val,w-wt[n-1],n-1)),knapsack(wt,val,w,n-1));
+
+    }
+    else if(wt[n-1]>w){
+        return (knapsack(wt,val,w,n-1));
+    }
+
+}
+int main(){
+    int n;
+    cin>>n;
+    int wt[n],val[n];
+    for(int i=0;i<n;i++){
+        cin>>wt[i];
+    }
+    for(int i=0;i<n;i++){
+        cin>>val[i];
+    }
+    int w;
+    cin>>w;
+    cout<<knapsack(wt,val,w,n);
+
+
+
+return 0;}
